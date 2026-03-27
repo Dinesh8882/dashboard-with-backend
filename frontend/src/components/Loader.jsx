8a6dda2176fb6830f11abb9ba4eff263eb5cba0d
dinesh8882 <dineshmahor0987@gmail.com>
2026-03-27 16:13:01 +0545

    text-loader

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -11,12 +11,12 @@ function Loader() {
         <div>
             {
                 loading ? (
-                     <div className="flex items-center justify-center h-screen bg-slate-900">
-      <div className="relative">
-        <div className="w-16 h-16 border-4 border-blue-400 rounded-full animate-ping absolute"></div>
-        <div className="w-16 h-16 border-4 border-blue-500 rounded-full"></div>
-      </div>
-    </div>
+                    <div className="absolute top-0 left-0 z-40 w-full flex items-center justify-center h-screen bg-slate-900">
+                        <div className="">
+                            <div className="w-16 h-16 border-4 border-blue-400 rounded-full animate-ping absolute"></div>
+                            <div className="w-16 h-16 border-4 border-blue-500 rounded-full"></div>
+                        </div>
+                    </div>
                 ) : (
                     <div></div>
                 )
